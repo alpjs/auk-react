46785c30c41107839967b4f1f321fa2dac502663
Christophe Hurpeau <christophe@hurpeau.com>
2016-01-19 22:08:21 +0100

    replace Component by View

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import render from 'fody';
 
 export default function aukReact(Html) {
     return (app) => {
-        app.context.render = function (Component, data) {
+        app.context.render = function (View, data) {
             this.body = render({
                 context: this,
-                Component,
+                View,
                 data,
                 Html,
             });
